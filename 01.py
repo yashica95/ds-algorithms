c34ce393ffc953ed31521360767fc77669dcc4a7
Yashica <yashica.iitd@gmail.com>
2022-04-21 23:13:44 -0400

    Day 1

---
+++ 01.py
@@ -0,0 +1,18 @@
+#1523. Count Odd Numbers in an Interval Range
+
+#Solution 1 - O(n) complexity
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        j = 0
+        for i in range(low, high+1):
+            if i%2!=0:
+                j +=1
+        return j
+
+#Solution 2 - O(1) complexity
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        if low%2 ==1 or high%2==1:
+            return (high- low)//2 + 1
+        else:
+            return (high- low)//2
