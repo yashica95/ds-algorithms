d5d4f41eef26fa912b0011d43e0f8feee5402e1b
Yashica <yashica.iitd@gmail.com>
2022-04-26 09:08:34 -0400

    Update 05.py

--- 05.py
+++ 05.py
@@ -10,38 +10,22 @@ Open brackets must be closed in the correct order.
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        list_accepted = ["()", "[]", "{}"]
-        #start from the middle
-        i = len(s)//2
-        while i>0:
-            i =len(s)//2
-            if i%2 ==0 :
+        pair_brackets = {
+        "(": ")",
+        "[" :"]",
+        "{" : "}"
+        }
 
-                print(i)
-                if s not in list_accepted:
-                    if (s[i] + s[i+1] in list_accepted):
-                        s = s[0 : i : ] + s[i + 2 : :]
-                        print(s)
-                        #i -=1
-                    else:
-                        i=0
-                        return False
-                else:
-                    i=0
-                    return True
-
+        stack =[]
+        for char in s:
+            print(stack)
+            if char in pair_brackets.keys():
+                stack.append(char)
+            elif char in pair_brackets.values():
+                if stack==[] or char != pair_brackets[stack.pop()]:
+                    return False
             else:
-                #print(i)
-                if s not in list_accepted:
-                    if (s[i-1] + s[i] in list_accepted):
-                        s = s[0 : i-1: ] + s[i + 1 : :]
-                        print(s)
-                    else:
-                        i=0
-                        return False
-                        #i -=1
-                else:
-                    i=0
-                    return True
+                return False
+        return stack==[]
 
 Solution().isValid("()()[]")
